27cac3d6f25e781a604a5a389f9e5cfc43279a2f
Francis Adeboye <adeboye.francis88@gmail.com>
2024-11-06 11:02:58 +0000

    updated route53 record and added dependencies

--- infra/acm.tf
+++ infra/acm.tf
@@ -5,23 +5,25 @@
 
 resource "aws_acm_certificate" "acm_certificate" {
   provider = aws.us-east-1
-  domain_name               = "touchedbyfrancis.cloud"
-  subject_alternative_names = ["*.touchedbyfrancis.cloud"]
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method         = "DNS"
 
   lifecycle {
     create_before_destroy = true
   }
 }
-
-# get details about a route 53 hosted zone
+###########################
+# Fetch Hosted Zone Details
+###########################
 data "aws_route53_zone" "route53_zone" {
-  name         = "touchedbyfrancis.cloud"
+  name         = var.domain_name
   private_zone = false
 }
-
+#########################################################
 # create a record set in route 53 for domain validatation
-resource "aws_route53_record" "route53_record" {
+#########################################################
+resource "aws_route53_record" "route53_valid" {
   for_each = {
     for dvo in aws_acm_certificate.acm_certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
@@ -36,15 +38,53 @@ resource "aws_route53_record" "route53_record" {
   ttl             = 60
   type            = each.value.type
   zone_id         = data.aws_route53_zone.route53_zone.zone_id
+  
 }
 
+#############################################
+# Route 53 Records for CloudFront Distribution
+#############################################
+
+resource "aws_route53_record" "records_for_cf" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
+}
+
+resource "aws_route53_record" "records_for_cf_www" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "www.${var.domain_name}"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = true
+  }
+
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
+}
+
+#############################################
 # validate acm certificates
+#############################################
 resource "aws_acm_certificate_validation" "acm_certificate_validation" {
-  certificate_arn         = aws_acm_certificate.acm_certificate.arn
-  validation_record_fqdns = [for record in aws_route53_record.route53_record : record.fqdn]
+  certificate_arn  = aws_acm_certificate.acm_certificate.arn
+  provider = aws.us-east-1
+  validation_record_fqdns = [for record in aws_route53_record.route53_valid : record.fqdn]
 
   depends_on = [
-    aws_route53_record.route53_record
+    aws_route53_record.route53_valid
   ]
 
 }
+
+
